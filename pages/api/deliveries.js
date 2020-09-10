ac7c82181c85cb0d1f9515d377016a0eb87add29
Marko Kvakić <markokvakic@gmail.com>
2020-09-10 05:35:34 +0200

    api/deliveries changed into a promise chain

--- pages/api/deliveries.js
+++ pages/api/deliveries.js
@@ -5,23 +5,28 @@ const handler = nextConnect();
 
 handler.use(middleware);
 
-handler.get(async (req, res) => {
+handler.get( (req, res) => {
 
   // filter by date + 2 days
   const date = new Date();
   date.setHours(0, 0, 0, 0);
   date.setDate(date.getDate() + 2);
 
-  // deliveries
-  const deliveries = await req.db
+  // send
+  req.db
     .collection('deliveries')
     .find({
       from: {
         $gte: new Date(date.toISOString()),
       }
     })
-    .toArray();
-  res.send(deliveries);
+    .toArray()
+    .then(deliveries => {
+      res.status(200).send({ deliveries });
+    })
+    .catch(err => {
+      res.status(500).send({ deliveries: null });
+    });
 
 });
 
