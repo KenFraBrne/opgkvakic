447b58cde3a4862ef1d3cfd5f49beea3a96eb511
Marko Kvakić <markokvakic@gmail.com>
2020-09-12 17:30:32 +0200

    user update api created

---
+++ pages/api/user/update.js
@@ -0,0 +1,42 @@
+import nextConnect from 'next-connect';
+import middleware from 'middleware';
+import passport from 'middleware/passport';
+import { ObjectId } from 'mongodb';
+
+const handler = nextConnect();
+
+handler.use(middleware);
+
+handler.post( (req, res) => {
+  if (req.isAuthenticated()){
+    const body = req.body;
+    const user = req.user;
+    const filter = { _id: user._id };
+    const update = { $set: body };
+    req.db
+      .collection('users')
+      .findOneAndUpdate(filter, update)
+      .then(ret => {
+        res.status(200).send({ user: ret.value });
+      });
+  } else {
+    res.status(403).end();
+  };
+});
+
+handler.delete((req, res) => {
+  if (req.isAuthenticated()){
+    req.db
+      .collection('users')
+      .findOneAndDelete({_id: req.user._id})
+      .then(ret => {
+        console.log(ret);
+        req.logout();
+        res.status(204).end();
+      })
+  } else {
+    res.status(403).end();
+  };
+});
+
+export default handler;
