62485c1fa040db46a1fb2b187e25e4ee0bdee0b4
Marko Kvakić <markokvakic@gmail.com>
2020-09-22 07:42:30 +0200

    gmail middleware without fs

--- middleware/gmail.js
+++ middleware/gmail.js
@@ -1,25 +1,27 @@
-const fs = require('fs/promises');
 const googleAuth = require('googleapis').google.auth;
 const googleGmail = require('googleapis').google.gmail;
 
-const TOKEN_PATH = 'gmail_token.json';
-
-async function getAuth(credentials){
-  const { client_secret, client_id, redirect_uris } = credentials.installed;
-  const auth = new googleAuth.OAuth2( client_id, client_secret, redirect_uris[0] );
-  await fs.readFile(TOKEN_PATH)
-    .then(token => {
-      auth.setCredentials(JSON.parse(token));
-    })
-    .catch(err => {
-      console.error('Token does not exist:', err);
-    });
+function getAuth(){
+  const client_id = process.env.GMAIL_CLIENT_ID;
+  const client_secret = process.env.GMAIL_CLIENT_SECRET;
+  const redirect_uri = process.env.GMAIL_REDIRECT_URI;
+  const access_token = process.env.GMAIL_ACCESS_TOKEN;
+  const expiry_date = Number( process.env.GMAIL_EXPIRY_DATE );
+  const refresh_token = process.env.GMAIL_REFRESH_TOKEN;
+  const scope = process.env.GMAIL_SCOPE;
+  const token_type = process.env.GMAIL_TOKEN_TYPE;
+  const auth = new googleAuth.OAuth2( client_id, client_secret, redirect_uri );
+  auth.setCredentials({
+    access_token,
+    expiry_date,
+    refresh_token,
+    token_type,
+  });
   return auth
 }
 
-async function gmail(req, res, next){
-  const credentials = await fs.readFile('gmail_credentials.json').then(JSON.parse);
-  const auth = await getAuth(credentials);
+function gmail(req, res, next){
+  const auth = getAuth();
   req.gmail = googleGmail({ version: "v1", auth });
   return next();
 }
