f95c790642a87ca1aad4d41ae7023a729b39f3d2
Marko Kvakić <markokvakic@gmail.com>
2020-09-23 08:08:46 +0200

    initial data load in _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'styles/globals.css'
 
+import getServerData from 'util/getServerData';
+
 function MyApp({ Component, pageProps }) {
+
+  // initial load
+  const [ load, setLoad ] = useState(true);
+  getServerData(load ? '/api/user' : null);
+  getServerData(load ? '/api/order' : null);
+  getServerData(load ? '/api/posts' : null);
+  getServerData(load ? '/api/products' : null);
+  getServerData(load ? '/api/deliveries' : null);
+  useEffect(() => {
+    setLoad(false);
+  }, []);
+
   return (
     <Component {...pageProps} />
   )
--- util/getServerData.js
+++ util/getServerData.js
@@ -4,10 +4,14 @@ const fetcher = (url) => fetch(url).then(res => res.json());
 
 function getServerData(key){
   const { data, mutate } = useSWR(key, fetcher);
-  const name = key.split('/').pop();
-  return {
-    [name]: data?.[name],
-    ['mutate' + name.charAt(0).toUpperCase() + name.slice(1)]: mutate,
+  if (key){
+    const name = key.split('/').pop();
+    return {
+      [name]: data?.[name],
+      ['mutate' + name.charAt(0).toUpperCase() + name.slice(1)]: mutate,
+    }
+  } else {
+    return {};
   }
 };
 
