16f17cff716691d954e6eee433ee18bf95414b72
Marko Kvakić <markokvakic@gmail.com>
2020-09-25 15:49:26 +0200

    homepage background images

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import Fade from 'react-reveal/Fade';
 
 import Link from 'next/link';
 
+import Container from 'react-bootstrap/Container';
+
 import MainLayout from 'layout/MainLayout'
 
 import styles from './styles.module.css';
@@ -16,26 +18,21 @@ export default function HomePage() {
         <div className={styles.row}>
           <div className={styles.colTxt}>
             <h2> Dobrodošli </h2>
-            <p> Pogledajte našu trenutnu <Link href="/order"><a>ponudu</a></Link> ili nas <Link href="/about"><a>upoznajte</a></Link>. </p>
+            <p>
+              Pogledajte našu trenutnu <Link href="/order"><a>ponudu</a></Link> ili
+              nas <Link href="/about"><a>upoznajte</a></Link>.
+            </p>
           </div>
-          <div className={styles.colImg}>
-            <img
-              srcSet={[
-                require("pages/images/intro/90901577_626949784816701_5041255869845602304_n.jpg?webp"),
-                require("pages/images/intro/90901577_626949784816701_5041255869845602304_n.jpg"),
-              ].join(', ')}/>
+          <div className={styles.colImg}
+            style={{ backgroundImage: 'url(/intro/90901577_626949784816701_5041255869845602304_n.jpg)' }}>
           </div>
         </div>
       </Fade>
 
       <Fade>
-        <div className={styles.rowRev}>
-          <div className={styles.colImg}>
-            <img
-              srcSet={[
-                require("pages/images/intro/WhatsApp_Image_2020-03-28_at_10.07.07.jpeg?webp"),
-                require("pages/images/intro/WhatsApp_Image_2020-03-28_at_10.07.07.jpeg"),
-              ].join(', ')}/>
+        <div className={styles.wor}>
+          <div className={styles.colImg}
+            style={{ backgroundImage: 'url(/intro/WhatsApp_Image_2020-03-28_at_10.07.07.jpeg)' }}>
           </div>
           <div className={styles.colTxt}>
             <h2> Dostave </h2>
