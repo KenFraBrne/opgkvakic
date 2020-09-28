db1dfd4a38002b1f07c7c253cfa777e357021588
Marko Kvakić <markokvakic@gmail.com>
2020-09-28 12:42:08 +0200

    navbar language change

--- component/Navbar/index.js
+++ component/Navbar/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 import Link from 'next/link';
 
+import { useContext } from 'react';
+import { LanguageContext } from 'context/Language';
+import * as types from 'context/Language/types';
+
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavLink from 'react-bootstrap/NavLink';
@@ -13,13 +17,51 @@ import { FiShoppingCart } from 'react-icons/fi';
 
 import getServerData from 'util/getServerData';
 
-const MojNavbar = () => {
+const MyNavbar = () => {
   
-  const { order } = getServerData('/api/order');
+  // load user
   const { user } = getServerData('/api/user');
   
   // cart color depending on order products
+  const { order } = getServerData('/api/order');
   const cartColor = order?.products?.reduce((tot, val) => tot+val.amount, 0) ? 'red' : 'inherit';
+
+  // language change
+  const { language, languageDispatch } = useContext(LanguageContext);
+  const handleLanguageSelect = (lang) => {
+    switch (lang){
+      case "en": return languageDispatch({ type: types.SET_EN });
+      case "fr": return languageDispatch({ type: types.SET_FR });
+      default:   return languageDispatch({ type: types.SET_HR });
+    };
+  };
+
+  // left nav links
+  const navLinkLeft = [
+    "/",
+    "/order",
+    "/posts",
+  ].map( (link, ind) => {
+    return (
+      <Link key={ind} href={link} passHref>
+        <NavLink>{language.content.component.Navbar.navLinkLeft[ind]}</NavLink>
+      </Link>
+    );
+  });
+
+  // dropdown links
+  const dropdownLinks = [
+    "/about",
+    "/about/how",
+    "/about/where",
+    "/about/contact",
+  ].map( (link, ind) => {
+    return (
+      <Link key={ind} href={link} passHref>
+        <NavDropdown.Item>{language.content.component.Navbar.NavDropdown.Link[ind]}</NavDropdown.Item>
+      </Link>
+    );
+  });
 
   return (
 
@@ -39,38 +81,22 @@ const MojNavbar = () => {
       <NavbarToggle aria-controls="responsive-navbar-nav" />
 
       <NavbarCollapse>
-        <Nav className="mr-auto">
+        <Nav className="mr-auto align-items-left align-items-sm-center">
 
-          <Link href="/" passHref>
-            <NavLink>Naslovna</NavLink>
-          </Link>
+          {navLinkLeft}
 
-          <Link href="/order" passHref>
-            <NavLink>Ponuda</NavLink>
-          </Link>
+          <NavDropdown title={language.content.component.Navbar.NavDropdown.title} id="nav-dropdown">
+            {dropdownLinks}
+          </NavDropdown>
 
-          <Link href="/posts" passHref>
-            <NavLink>Novosti</NavLink>
-          </Link>
-
-          <NavDropdown title="O nama" id="nav-dropdown">
-
-            <Link href="/about" passHref>
-              <NavDropdown.Item>Tko smo</NavDropdown.Item>
-            </Link>
-
-            <Link href="/about/how" passHref>
-              <NavDropdown.Item>O dostavama</NavDropdown.Item>
-            </Link>
-
-            <Link href="/about/where" passHref>
-              <NavDropdown.Item>Kako do nas</NavDropdown.Item>
-            </Link>
-
-            <Link href="/about/contact" passHref>
-              <NavDropdown.Item>Kontakt</NavDropdown.Item>
-            </Link>
-
+          <NavDropdown
+            id="nav-language"
+            title={language.icon}
+            onSelect={handleLanguageSelect}
+            style={{ fontSize: "1.5em" }}>
+            <NavDropdown.Item eventKey="hr"> 🇭🇷 Hrvatski </NavDropdown.Item>
+            <NavDropdown.Item eventKey="en"> 🇬🇧 English </NavDropdown.Item>
+            <NavDropdown.Item eventKey="fr"> 🇫🇷 Français </NavDropdown.Item>
           </NavDropdown>
 
         </Nav>
@@ -82,7 +108,6 @@ const MojNavbar = () => {
             </NavLink>
           </Link>
           
-
           { user ? 
             <Link href="/user" passHref>
               <NavLink as="a" className="pr-3">
@@ -91,7 +116,7 @@ const MojNavbar = () => {
             </Link> :
             <Link href="/login" passHref>
               <NavLink as="a" className="pr-3">
-                Prijava
+                {language.content.component.Navbar.navLinkRight}
               </NavLink>
             </Link> }
 
@@ -102,4 +127,4 @@ const MojNavbar = () => {
   );
 };
 
-export default MojNavbar;
+export default MyNavbar;
