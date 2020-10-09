f5bff58b638034377636618a3f7d2514a21fd4ef
Marko Kvakić <markokvakic@gmail.com>
2020-10-09 13:57:47 +0200

    order api removal of nonexisting products

--- pages/api/order.js
+++ pages/api/order.js
@@ -1,13 +1,28 @@
 import nextConnect from 'next-connect';
 import middleware from 'middleware';
 import passport from 'middleware/passport';
+import { ObjectId } from 'mongodb';
 
 const handler = nextConnect();
 
 handler.use(middleware);
 
-handler.get( (req, res) => {
+handler.get(async (req, res) => {
   const order = req.session.order || null;
+  if (order?.products) {
+    // filter products which do not exist anymore (via mask)
+    const ids = order.products.map( product => product._id );
+    const promises = ids.map( _id => {
+      return req.db
+        .collection('products')
+        .findOne({ _id: new ObjectId(_id) })
+        .then(ret => !!ret);
+    });
+    const mask = await Promise.all(promises);
+    const products = order.products.filter( ( _, ind ) => mask[ind]);
+    // replace products in order
+    order.products = products;
+  };
   res.status(200).send({ order });
 });
 
